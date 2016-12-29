2349bd43686dd5c42ce35d56bddde48f889113b7
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-12-29 17:19:21 +0100

    Added refresh dependencies to gradle

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo -e "Checking Camden"
-./gradlew clean checkMappings checkGeneratedTests -PverifierVersion=1.0.3.BUILD-SNAPSHOT -PBOM_VERSION=Camden.BUILD-SNAPSHOT --stacktrace
+./gradlew clean checkMappings checkGeneratedTests -PverifierVersion=1.0.3.BUILD-SNAPSHOT  -PBOM_VERSION=Camden.BUILD-SNAPSHOT --stacktrace --refresh-dependencies
 
 echo -e "\n\nChecking Dalston"
-./gradlew clean checkMappings checkGeneratedTests -PverifierVersion=1.1.0.BUILD-SNAPSHOT -PBOM_VERSION=Dalston.BUILD-SNAPSHOT --stacktrace
+./gradlew clean checkMappings checkGeneratedTests -PverifierVersion=1.1.0.BUILD-SNAPSHOT -PBOM_VERSION=Dalston.BUILD-SNAPSHOT --stacktrace --refresh-dependencies
